212c1800484186c77669f850eededf7204497a37
Douglas A B Novato <douglasabnovato@gmail.com>
2022-09-06 08:27:24 -0300

    comunicação indireta

--- src/components/contador/PassoForm.jsx
+++ src/components/contador/PassoForm.jsx
@@ -2,11 +2,15 @@
 import React from "react";
 
 export default function PassoForm(props) {
-    return (
-        <div>
-            <label htmlFor="passoInput">Passo: </label>
-            <input id="passoInput" type="number" 
-            value={props.passo} onChange={e => props.setPasso(+e.target.value)}/>
-        </div> 
-    )
+  return (
+    <div>
+      <label htmlFor="passoInput">Passo: </label>
+      <input
+        id="passoInput"
+        type="number"
+        value={props.passo}
+        onChange={(e) => props.setPasso(+e.target.value)}
+      />
+    </div>
+  );
 }
